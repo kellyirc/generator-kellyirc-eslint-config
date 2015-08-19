e507b333244e94cfdb470a48a41a0388a912b8dc
Raymond Hammarling <raykay.rh@gmail.com>
2015-08-19 19:50:55 +0200

    Initial commit

---
+++ generators/app/index.js
@@ -0,0 +1,23 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+    prompting: function () {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            "Generator for kellyirc's eslint config!"
+        ));
+    },
+
+    writing: function () {
+        this.fs.copy(
+            this.templatePath('.eslintrc'),
+            this.destinationPath('.eslintrc')
+        );
+    },
+
+    install: function () {
+        this.npmInstall(['eslint-config-kellyirc', 'babel-eslint'], { saveDev: true });
+    }
+});
